da15d75e9c1daa34d0a261c60a0b82bcdf97b20c
Varsha-rathi <varshaor24@gmail.com>
2021-05-18 02:07:45 +0530

    logo added

--- src/core/header.jsx
+++ src/core/header.jsx
@@ -5,7 +5,15 @@ class Header extends React.Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark" fixed="top" >
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="/">
+                <img
+                    src="../assets/images/home/logo.png"
+                    width="90"
+                    height="40"
+                    className="d-inline-block align-top"
+                    alt="CanSat"
+                />
+                </Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/mission">Mission</Nav.Link>
