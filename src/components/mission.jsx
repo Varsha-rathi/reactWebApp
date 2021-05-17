335d8f58c91ba86ace078cd6fc73bc6e2a1aca37
Varsha-rathi <varshaor24@gmail.com>
2021-05-18 01:50:44 +0530

    updated for mission page

--- src/components/mission.jsx
+++ src/components/mission.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Carousel, Jumbotron, Container, Row, Col} from 'react-bootstrap';
+import {Carousel, Jumbotron, Container, Row, Col, Card} from 'react-bootstrap';
+import missionconst from '../constants/missiondata';
 
 import { Transition } from 'react-transition-group';
 class Mission extends React.Component {
@@ -14,60 +15,38 @@ class Mission extends React.Component {
         };
         return (
             <div>
-                <Jumbotron fluid className="team">
-                    <Container className="text-center">
-                        <Transition in={true} timeout={duration} appear>
-                            {(state) => (
-                                <div
-                                    style={{
-                                        ...defaultStyle,
-                                        ...transitionStyles[state]
-                                    }}
-                                >
-                                    <h3>Meet Our Team</h3>
-                                    
-                                </div>
-                            )}
-                        </Transition>
-                        <br />
-                    </Container>
+                <Jumbotron fluid className="mission">
                 </Jumbotron>
-                <Container>
-                        <Teamview />
-                </Container>
-            </div>
-            <div>
-            <Jumbotron fluid className="mission">
 
-                    <Container className="text-center">
+                <Container className="text-center mt-5">
+                    <h4 class="text-center text-uppercase">Our Mission</h4><hr class="dashed"></hr>
                     <Row>
-                        <Col>
-                        <Transition in={true} timeout={900} appear>
-
-                                        {(state) => (
-                                        
-                                        <Col xl={12} xs={12} className="border rounded p-3" style={{
-                                            ...defaultStyle,
-                                            ...transitionStyles[state],
-                                            transition: `900ms ease-in`
-                                        }}>
-                                            
-                                            <div className="bg-white rounded shadow-sm py-5 px-4">
-                                                <h6 class="text-center text-uppercase">Overview</h6>
-                                                <hr class="dashed"></hr>
-                                                Much like Semantic UI, you can use Bootstrap — a great responsive mobile-first CSS framework — to build an aesthetically pleasing app in much less time than if you would write your own CSS. We want components, grids and pretty buttons after all!
-                                            </div>
-                                        </Col>
-                                    )}
-                                    </Transition>
-                        </Col>
+                        {
+                            missionconst.map((data) => 
+                            <Transition in={true} timeout={900} appear>
+                                {(state) => (
+                                    <Col xl={4} xs={4} className={`d-flex p-3 ${data.class}`} style={{
+                                        ...defaultStyle,
+                                        ...transitionStyles[state],
+                                        transition: `900ms ease-in`
+                                    }}>
+                                        <Card className="flex-fill">
+                                            <Card.Body>
+                                                <Card.Title><h6 class="text-center text-uppercase">{data.title}</h6><hr class="dashed"></hr></Card.Title>
+                                                <Card.Text>
+                                                    <p class="para">{data.text}</p>
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                )}
+                            </Transition>
+                            )
+                        }
                     </Row>
-                    
-                    
-                    </Container>
+                </Container>
 
-                </Jumbotron>
-                </div>
+            </div>
         );
     }
 }
