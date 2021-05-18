44d750b1dc3da684b5764266d638675d260c0a43
mansha21 <mansha.gandhi2020@vitstudent.ac.in>
2021-05-19 00:36:20 +0530

    updated

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -55,7 +55,22 @@ class Landing extends React.Component {
                                             <div className="bg-white rounded shadow-sm py-5 px-4">
                                                 <h6 class="text-center text-uppercase">Overview</h6>
                                                 <hr class="dashed"></hr>
-                                                Much like Semantic UI, you can use Bootstrap — a great responsive mobile-first CSS framework — to build an aesthetically pleasing app in much less time than if you would write your own CSS. We want components, grids and pretty buttons after all!
+                                                <p className="p-2 justify-content">
+                                                Our Cansat is built to perform multiple tasks as per the mission statement. The Cansat consists of two parts: carrier and science payload. The science payload further separates into two parts; heatshield and probe. The carrier is the backbone of the cansat. It safely deploys the science payload and also acts like a medium for the probe to relay data to the ground station. The heatshield is designed to reduce the descent rate of the science payload. While the probe is the brain of the cansat; it collects local weather data and sends it to the carrier. The probe also encloses a hen’s egg which shouldn’t crack during the course of the mission.
+                                                The main objectives our Cansat has to accomplish are:</p>
+                                                <hr class="dashed"></hr>
+                                                <p className="p-2 justify-content">Our Cansat will be loaded inside a sounding rocket and launched. The Cansat would then be ejected from the rocket at 2000m from ground. The carrier (consisting of the science payload) has to maneuver helically downward for 500m upon release. The helical maneuver would be performed with foldable wings attached on the carrier. During this the maneuver, the carrier would also collect data such as mission time, altitude, orientation and GPS coordinates and transmit it to the ground station.</p>
+                                                <hr class="dashed"></hr>
+                                                <p className="p-2 justify-content"> Once the carrier attains altitude of 1500m, it starts its powered flight using a propeller attached on the tip of the nose cone to move 300m east.Once the carrier reaches its destination it releases the science payload. The science payload then descends at a rate of approximately 15m/s for 1000m with the help of a specially designed heat shield. The probe in the science payload collects local weather data such as air temperature, altitude and air speed and also protects the hen’s egg.</p>
+                                                <hr class="dashed"></hr>
+                                                <p className="p-2 justify-content">Once the science payload reaches 500m from the ground, the heatshield separates from the probe. The probe then descends at 5m/s using a parachute.
+                                                The carrier stops its powered flight and lands being in a gliding state.</p>
+                                                <hr class="dashed"></hr>
+                                                <p className="p-2 justify-content">Additional objectives:
+                                                <li>The carrier, heatshield, probe and the egg should be located and retrieved.</li>
+                                                <li>The data received should be plotted in real time and recorded in the ground station.</li>
+                                                </p>
+
 
                                                 <Row className="mt-3 p-3">
                                                     <Col xs={6}>
